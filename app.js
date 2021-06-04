c49bbf7a95b1bfe3fc0d9056c2340ca7b3f0440d
Soravit Yenjit <soravit.m@gmail.com>
2021-06-04 22:18:41 +0700

    update cors

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require('express') // ใช้ framework express.js
 const bodyParser = require('body-parser')
+var cors = require('cors')
 
 const app = express();
 const cars = require('./route/cars')
 
 // parse application/json
 app.use(bodyParser.json())
+
+// allow cross origin header all route
+app.use(cors())
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
