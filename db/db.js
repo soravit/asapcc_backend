23b787cc65ab7abe31a20e12d3536343f97a82a0
Your Name <you@example.com>
2021-08-06 19:49:28 +0700

    a

--- db/db.js
+++ db/db.js
@@ -1,11 +1,11 @@
 const db = require('mysql2')
 
 const Database = db.createPool({
-    host:'128.199.155.179',
-    user:'asapcallcenter',
-    database:'asapcallcenter',
-    password:'treetreerice',
-    port:3306
+    host:'203.151.205.217',
+    user:'asap',
+    database:'asapapp',
+    password:'asap',
+    port:3307
 })
 
 module.exports = Database.promise();
